7afae4b29ca298a65f33199d411a85a072edb115
maxfadeev <oxinhood@gmail.com>
2017-03-29 10:31:43 +0300

    Remove semicolons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
 ReactDOM.render(
   <h1>bullsandcows</h1>,
   document.getElementById('root')
-);
+)
