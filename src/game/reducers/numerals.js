a0d352d943994d2ba4b6a783bf0a61e8e36705a0
maxfadeev <masdeft@gmail.com>
2017-04-11 17:37:55 +0300

    Handle CLICK_SCORE_BUTTON action

--- src/game/reducers/numerals.js
+++ src/game/reducers/numerals.js
@@ -1,13 +1,15 @@
 import numericButtons from './numericButtons'
 import guessDigits from './guessDigits'
-import { CLICK_NUMERIC_BUTTON, CLICK_GUESS_DIGIT } from '../constants/ActionTypes'
+import scores from './scores'
+import { CLICK_NUMERIC_BUTTON, CLICK_GUESS_DIGIT, CLICK_SCORE_BUTTON } from '../constants/ActionTypes'
 import { GUESS_DIGITS_LENGTH } from '../constants/Main'
 
 const numerals = (state, action) => {
   if (typeof state === 'undefined') {
     return {
       numericButtons: numericButtons(undefined, action),
-      guessDigits: guessDigits(undefined, action)
+      guessDigits: guessDigits(undefined, action),
+      scores: scores(undefined, action)
     }
   }
 
@@ -30,8 +32,18 @@ const numerals = (state, action) => {
           state.guessDigits, 
           action,
           isDigitAvailable
-        )
+        ),
+        scores: state.scores
       }
+    case CLICK_SCORE_BUTTON:
+      if (state.guessDigits.length === GUESS_DIGITS_LENGTH) {
+        return {
+          numericButtons: numericButtons(undefined, action),
+          guessDigits: guessDigits(undefined, action),
+          scores: scores(state.scores, action)
+        }
+      }
+      return state
     default:
       return state
   }
