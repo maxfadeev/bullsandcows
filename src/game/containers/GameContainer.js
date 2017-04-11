09b164b3f66a1a715b0d8c1759bf380f8d27504d
maxfadeev <masdeft@gmail.com>
2017-04-11 16:38:24 +0300

    Create a container of ScoreButton and put it to GameContainer

--- src/game/containers/GameContainer.js
+++ src/game/containers/GameContainer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import NumericButtonsListContainer from './NumericButtonsListContainer'
 import GuessContainer from './GuessContainer'
+import ScoreButtonContainer from './ScoreButtonContainer'
 
 const GameContainer = () => (
   <div>
     <NumericButtonsListContainer />
     <GuessContainer />
+    <ScoreButtonContainer />
   </div>
 )
 
---
+++ src/game/containers/ScoreButtonContainer.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux'
+
+import ScoreButton from '../components/ScoreButton'
+import { clickScoreButton } from '../actions'
+
+const mapStateToProps = (state) => {
+  return {
+    guessDigits: state.numerals.guessDigits
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onClick: (guessDigits) => {
+      dispatch(clickScoreButton(guessDigits))
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ScoreButton)
