e145eac8302665146a0f0e9cdc87cc2d6fb92a84
maxfadeev <oxinhood@gmail.com>
2017-05-29 17:11:28 +0300

    Enable redux devtools

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reducer from './game/reducers'
 import GameContainer from './game/containers/GameContainer'
 
 ReactDOM.render(
-  <Provider store={createStore(reducer)}>
+  <Provider store={createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
     <GameContainer />
   </Provider>,
   document.getElementById('root')
