89c50d1aeabaf7f86c71c92d4510d38c801e9917
maxfadeev <oxinhood@gmail.com>
2017-05-30 12:10:25 +0300

    Change a score button text depending on the turn

--- src/game/components/ScoreButton.js
+++ src/game/components/ScoreButton.js
@@ -1,4 +1,7 @@
 import React from 'react'
+
+import { GUESS_TURN, SCORE_TURN } from '../constants/Game'
+
 import '../styles/scoreButton.css'
 
 const ScoreButton = ({
@@ -8,8 +11,9 @@ const ScoreButton = ({
   player2TypedDigits,
   isScoreButtonAvailable
 }) => {
+  const text = turn === GUESS_TURN ? 'Guess' : 'Score'
   if (isScoreButtonAvailable === false) {
-    return <span className="score-button-stub disable-select">Score</span>
+    return <span className="score-button-stub disable-select">{text}</span>
   }
   return (
     <a href="#"
@@ -19,7 +23,7 @@ const ScoreButton = ({
         onClick(turn, player1TypedDigits, player2TypedDigits)
       }}
     >
-      Score
+      {text}
     </a>
   )
 }
