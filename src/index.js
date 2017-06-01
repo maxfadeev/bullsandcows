83fcb72cea933ff0398c9af2a58275fca1542fb6
maxfadeev <oxinhood@gmail.com>
2017-06-01 17:42:27 +0300

    Add basic routes

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import { createStore } from 'redux'
 import './index.css'
 
 import reducer from './reducer'
-import GameContainer from './game/containers/GameContainer'
+import Pages from './pages'
 
 ReactDOM.render(
   <Provider store={createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
-    <GameContainer />
+    <Pages />
   </Provider>,
   document.getElementById('root')
 )
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Header from './components/Header'
+
+const Pages = () => (
+  <div>
+    <Header />
+  </div>
+)
+
+export default Pages
