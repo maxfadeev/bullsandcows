2506e90a71003e6fc412666ea022d99f1f0a7123
maxfadeev <masdeft@gmail.com>
2017-06-02 09:18:52 +0300

    Edit the Navigation

--- src/pages/components/Navigation.js
+++ src/pages/components/Navigation.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const Navigation = () => (
   <nav className="nav">
     <ul>
+      <li><Link to="/">Home</Link></li>
       <li><Link to="/about">About</Link></li>
       <li><Link to="/contact">Contact</Link></li>
       <li><Link to="/game">Game</Link></li>
