7db03704b6148929c22238cada0a1b5a48ae13af
maxfadeev <masdeft@gmail.com>
2017-06-05 16:25:24 +0300

    Add an image to the home page

--- src/pages/components/Home.js
+++ src/pages/components/Home.js
@@ -8,8 +8,12 @@ const Home = () => (
     <div className="home">
       <h2>Welcome!</h2>
       <p>My name is Max Fadeev. I am a Developer. Read a little bit more about
-        me <Link to="/about">here</Link>.</p>
+        me <Link to="/about">here</Link>.
+        If you don't need the information about me, you can just play a 
+        little <Link to="/game">game</Link>.</p>
       <p>Fill free to <Link to="/contact">contact</Link> me.</p>
+      <p>Here is a desert, me and an orange =).</p>
+      <img src="/img/desert.jpg" alt="desert" />
     </div>
   </DocumentTitle>
 )
