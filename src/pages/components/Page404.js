249d0f6f33e20791fd121ba4244f4eba9184c937
maxfadeev <oxinhood@gmail.com>
2017-06-07 16:37:09 +0300

    Add 404 page

---
+++ src/pages/components/Page404.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DocumentTitle from 'react-document-title'
+import { Link } from 'react-router-dom'
+import '../styles/page404.css'
+
+const Page404 = () => (
+  <DocumentTitle title="Page not found">
+    <div className="page404">
+      <h2>404</h2>
+    </div>
+  </DocumentTitle>
+)
+
+export default Page404
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
 
 import Header from './components/Header'
@@ -11,17 +12,21 @@ import About from './components/About'
 import Contact from './components/Contact'
 import Game from './components/Game'
 import Footer from './components/Footer'
+import Page404 from './components/Page404'
 import './styles/content.css'
 
 const Pages = () => (
   <Router>
     <div>
-      <Header />
+      <Route exact path="(/|/about|/game|/contact)" component={Header} />
       <main className="content">
-        <Route exact path="/" component={Home} />
-        <Route path="/game" component={Game} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/game" component={Game} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route component={Page404} />
+        </Switch>
         <Footer />
       </main>
     </div>
