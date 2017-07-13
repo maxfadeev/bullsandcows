0cc3f0f04fe34318d7d1fd56c10f49b20ab07157
max.fadeev <max.fadeev>
2017-07-13 08:36:43 +0100

    Take away score results from the score info

--- src/game/components/ScoreInfo.js
+++ src/game/components/ScoreInfo.js
@@ -4,13 +4,7 @@ import Message from './Message'
 
 const ScoreInfo = ({score}) => (
   <Message type="success">
-    Your score is  
-    <span className="game-info-result">
-      <b>{score[0]}</b><span className="green-icon"></span>
-    </span>
-    <span  className="game-info-result">
-      <b>{score[1]}</b><span className="orange-icon"></span>
-    </span>
+    Your score is bottommost-right in the table below
   </Message>
 )
 
