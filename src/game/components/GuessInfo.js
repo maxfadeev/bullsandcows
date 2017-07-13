9922dd16e249097d9e66ac464c0a57679a3338e8
max.fadeev <max.fadeev>
2017-07-13 08:34:02 +0100

    Wrap a guess info into <p>

--- src/game/components/GuessInfo.js
+++ src/game/components/GuessInfo.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Message from './Message'
 
 const GuessInfo = ({guess}) => (
-  <Message type="success">Your opponent's guess is <b>{guess}</b></Message>
+  <Message type="success">Your opponent's guess is <p><b>{guess}</b></p></Message>
 )
 
 export default GuessInfo
