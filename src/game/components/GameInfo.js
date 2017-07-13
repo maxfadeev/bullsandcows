c1d38473e8ab2c0a9ba13a1755e73ee2c3539a26
max.fadeev <max.fadeev>
2017-07-13 08:37:47 +0100

    Refactor GameInfo component

--- src/game/components/GameInfo.js
+++ src/game/components/GameInfo.js
@@ -9,14 +9,19 @@ import { GUESS_TURN, SCORE_TURN } from '../constants/Game'
 import '../styles/gameInfo.css'
 
 const GameInfo = ({result, turn}) => {
+  let info = (
+    <Message type="success">
+      Click on the numeric buttons above to make your guess
+    </Message>
+  )
   if (result && turn === SCORE_TURN) {
-    return <GuessInfo guess={result}></GuessInfo>
+    info = <GuessInfo guess={result}></GuessInfo>
   }
   else if (result && turn === GUESS_TURN) {
-    return <ScoreInfo score={result}></ScoreInfo>
+    info = <ScoreInfo score={result}></ScoreInfo>
   }
 
-  return <Message type="success">Click on the numeric buttons above to make your guess</Message>
+  return info
 }
 
 export default GameInfo
