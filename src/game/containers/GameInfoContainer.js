473370c0f75a0b7cf7f29db22c7f2579cf560083
max.fadeev <max.fadeev>
2017-07-13 08:47:14 +0100

    Refactor GameInfoContainer

--- src/game/containers/GameInfoContainer.js
+++ src/game/containers/GameInfoContainer.js
@@ -5,19 +5,20 @@ import GameInfo from '../components/GameInfo'
 import { SCORE_TURN, GUESS_TURN } from '../constants/Game'
 
 const mapStateToProps = (state) => {
+  const turn = state.game.turn
+  const guesses = state.game.guesses
+  const scores = state.game.scores
   let result = ''
-  if (state.game.turn === SCORE_TURN) {
-    const player2Guesses = state.game.guesses.player2
-    result = player2Guesses[player2Guesses.length - 1]  
+  if (turn === SCORE_TURN) {
+    result = guesses.player2[guesses.player2.length - 1]  
   } 
-  else if (state.game.turn === GUESS_TURN && state.game.guesses.player1.length !== 0) {
-    const player2Scores = state.game.scores.player2
-    result = player2Scores[player2Scores.length - 1] 
+  else if (turn === GUESS_TURN && guesses.player1.length !== 0) {
+    result = scores.player2[scores.player2.length - 1] 
   }
 
   return {
     result,
-    turn: state.game.turn
+    turn
   }
 }
 
